3df88f6b4ca88c84973bee137672684e9dc5fb3e
Marina Kuklina <nelmare@yandex.ru>
2021-11-07 14:10:25 +0300

    добавляет код добавления превью фотографий пользователя

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ import './display-ads.js';
 import './form-filter-clear.js';
 import './form-submit-message.js';
 import './api.js';
+import './preview.js';
 
 import {setUserFormSubmit} from './form-submit-message.js';
 import {onSubmitClearData} from './form-filter-clear.js';
---
+++ js/preview.js
@@ -0,0 +1,38 @@
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+const avatarFileChooser = document.querySelector('.ad-form__field input[type=file]');
+const avatarPreviewContainer = document.querySelector('.ad-form-header__preview');
+const avatarPreview = avatarPreviewContainer.querySelector('img');
+const photoFileChooser = document.querySelector('.ad-form__upload input[type=file]');
+const photoPreviewContainer = document.querySelector('.ad-form__photo');
+
+avatarFileChooser.addEventListener('change', () => {
+  const file = avatarFileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    avatarPreview.src = URL.createObjectURL(file);
+  }
+});
+
+photoFileChooser.addEventListener('change', () => {
+  const file = photoFileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+  if (matches) {
+    const housingPhoto = document.createElement('img');
+    housingPhoto.setAttribute('src', URL.createObjectURL(file));
+    housingPhoto.setAttribute('accept', 'image/png, image/jpeg');
+    housingPhoto.width = 69;
+    housingPhoto.height = 69;
+    photoPreviewContainer.style.display = 'flex';
+    housingPhoto.style.marginRight = '10px';
+    photoPreviewContainer.appendChild(housingPhoto);
+  }
+});
+
+
